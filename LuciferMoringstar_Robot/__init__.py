b659e0f616eb9ebc49835facacb98c861c21f6c5
Don_Sflix <92365864+GYGGFDUSHSH@users.noreply.github.com>
2021-10-31 20:42:28 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -28,14 +28,14 @@ Bot Owner Only
 """
 
 ABOUT = """
-➥ Name :  Auto Filter V3
+➥ Name :  Saitama
     
-➥ Creator : [Naughty PROFESSOR](
-https://youtube.com/c/NaughtyPROFESSOR)
+➥ Creator : [@Don_Sflix](
+https://t.me/Sflix2k)
 
 ➥ Language : Python3
 
 ➥ Library : Pyrogram Asyncio 1.13.0
 
-➥ Tutorial : [Click Me](https://youtu.be/5hnYOKBzyi8)
+➥ Our Group : [Click Here](https://t.me/Sflix2kchats)
 """
