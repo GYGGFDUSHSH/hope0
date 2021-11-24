4de4ba238de2107a761319c9d2a6993a493ea2ef
Don_Sflix <92365864+GYGGFDUSHSH@users.noreply.github.com>
2021-11-24 21:07:48 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -53,7 +53,7 @@ async def filter(client, message):
     if 2 < len(message.text) < 100:    
         btn = []
         search = message.text
-        mo_tech_yt = f"**🏷 Title:** {search}\n**⭐ Rating:** {random.choice(RATING)}\n** Genre:** {random.choice(GENRES)}\n**By Sflix💫"
+        mo_tech_yt = f"**🏷 Title:** {search}\n**⭐ Rating:** {random.choice(RATING)}\n** Genre:** {random.choice(GENRES)}\n** releaseinfo>{random.choice(year)}/n**By Sflix💫"
         files = await get_filter_results(query=search)
         if files:
             for file in files:
@@ -115,7 +115,7 @@ async def group(client, message):
     if 2 < len(message.text) < 50:    
         btn = []
         search = message.text
-        mo_tech_yt = f"**🏷️ Title:** {search}\n**⭐ Rating:** {random.choice(RATING)}\n** Genre:** {random.choice(GENRES)}\n** By Sflix💫**"
+        mo_tech_yt = f"**🏷️ Title:** {search}\n**⭐ Rating:** {random.choice(RATING)}\n** Genre:** {random.choice(GENRES)}\n** releaseinfo>{random.choice(year)}/n By Sflix💫**"
         nyva=BOT.get("username")
         if not nyva:
             botusername=await client.get_me()
