96ae22efd72f3157372a1261a82121f9a5e65225
Don_Sflix <92365864+GYGGFDUSHSH@users.noreply.github.com>
2021-12-20 18:36:04 +0530

    Update mt_botz.py

--- mt_botz.py
+++ mt_botz.py
@@ -3,13 +3,16 @@ import logging.config
 
 # Get logging configurations
 logging.config.fileConfig('logging.conf')
-logging.getLogger().setLevel(logging.ERROR)
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("imdbpy").setLevel(logging.ERROR)
 
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
-from LuciferMoringstar_Robot import Media
-from Config import SESSION, API_ID, API_HASH, BOT_TOKEN
-import pyromod.listen
+from database.ia_filterdb import Media
+from database.users_chats_db import db
+from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR
+from utils import temp
 
 class Bot(Client):
 
@@ -20,20 +23,27 @@ class Bot(Client):
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
             workers=50,
-            plugins={"root": "LuciferMoringstar_Robot"},
+            plugins={"root": "plugins"},
             sleep_threshold=5,
         )
 
     async def start(self):
+        b_users, b_chats = await db.get_banned()
+        temp.BANNED_USERS = b_users
+        temp.BANNED_CHATS = b_chats
         await super().start()
         await Media.ensure_indexes()
         me = await self.get_me()
+        temp.ME = me.id
+        temp.U_NAME = me.username
+        temp.B_NAME = me.first_name
         self.username = '@' + me.username
-        print(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
+        logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
+        logging.info(LOG_STR)
 
     async def stop(self, *args):
         await super().stop()
-        print("Bot stopped. Bye.")
+        logging.info("Bot stopped. Bye.")
 
 
 app = Bot()
