f4029345f869306fe039b172b06fa171435a4915
Don_Sflix <92365864+GYGGFDUSHSH@users.noreply.github.com>
2021-12-20 18:52:12 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,34 +1,23 @@
 # Bot information
-SESSION = 'LuciferMoringstar_Robot'
+SESSION = 'Media_search'
 USER_SESSION = 'User_Bot'
 API_ID = 12345
 API_HASH = '0123456789abcdef0123456789abcdef'
-BOT_TOKEN = '123456:LuciferMoringstar_Robot-zyx57W2v1u123ew11'
-USERBOT_STRING_SESSION = 'LuciferMoringstar_Robot'
+BOT_TOKEN = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
+USERBOT_STRING_SESSION = ''
 
 # Bot settings
 CACHE_TIME = 300
 USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
-ADMINS = [12345789, 'admin123', 987654321]
-CHANNELS = [-10012345678, -100987654321, 'Mo_Tech_YT']
+ADMINS = [12345789, 'admin123', 98765432]
+CHANNELS = [-10012345678, -100987654321, 'channelusername']
 AUTH_USERS = []
 AUTH_CHANNEL = None
 
 # MongoDB information
-DATABASE_URI = "mongodb://[LuciferMoringstar_Robot:LuciferMoringstar_Robot@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
-DATABASE_NAME = 'LuciferMoringstar_Robot'
+DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
+DATABASE_NAME = 'Telegram'
 COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
-# Messages
-START_MSG = """
-**My Name is Saitama
-I Am Moveis Files Manager Of Sflix
-You Want to Create Bots like This Message my Owner @Don_Sflix**
-
-Here you can search files in inline mode. Just press follwing buttons and start searching.
-"""
-
-SHARE_BUTTON_TEXT = 'Checkout {username} for searching files'
-INVITE_MSG = 'Please join @.... to use this bot'
